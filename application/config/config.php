700653244ead4becee632b79c4544fb5f3ad0934
Habib ur Rehaman <rehmank360@gmail.com>
2016-05-06 12:23:38 +0500

    dynamic host name added to config

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/socialmedia/';
+$config['base_url'] = $_SERVER['HTTP_HOST'].'/socialmedia/';
 
 /*
 |--------------------------------------------------------------------------
